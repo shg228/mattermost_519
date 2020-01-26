e40daf4fde7a04da8f3a6787cecd4b31f2a81cd9
Unknown <unknown@example.com>
2020-01-26 23:27:39 +0330

    fix footer text and dispaly of links

--- components/header_footer_template/header_footer_template.jsx
+++ components/header_footer_template/header_footer_template.jsx
@@ -106,7 +106,7 @@ export default class NotLoggedIn extends React.PureComponent {
                                 id='company_name'
                                 className='pull-right footer-site-name'
                             >
-                                {'Mattermost'}
+                                
                             </span>
                         </div>
                         <div className='col-xs-12'>
@@ -114,7 +114,7 @@ export default class NotLoggedIn extends React.PureComponent {
                                 id='copyright'
                                 className='pull-right footer-link copyright'
                             >
-                                {`© 2015-${new Date().getFullYear()} Mattermost, Inc.`}
+                                
                             </span>
                             <span className='pull-right'>
                                 {content}
